c38f2ec0ccead6a16b95eeefefc013fe9312c3bb
François Magimel <magimel.francois@gmail.com>
2020-04-24 19:55:19 +0200

    PS: env vars

--- .appveyor/before_build.ps1
+++ .appveyor/before_build.ps1
@@ -2,30 +2,30 @@
 
 # PATH: add dumpbin
 # https://www.appveyor.com/docs/lang/cpp/
-set PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin
+set PATH=$env:PATH;"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"
 
 # set the arch
-If (%platform% -eq "x64") {
+if ($env:platform -eq "x64") {
     set varsall="amd64"  # x86_amd64
-} Else {
+} else {
     set varsall="x68"
 }
 #call "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /%platform%
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %varsall%
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" $env:varsall
 
 # vcpkg: install dependencies
-vcpkg install glib:%platform%-windows
-              libogg:%platform%-windows
-              libtheora:%platform%-windows
-              ffmpeg:%platform%-windows
-#vcpkg export  glib:%platform%-windows
-#              libogg:%platform%-windows
-#              libtheora:%platform%-windows
-#              ffmpeg:%platform%-windows --zip
-#cp c:\Tools\vcpkg\"vcpkg-export-*.zip" %APPVEYOR_BUILD_FOLDER%
+vcpkg install glib:$env:platform-windows
+              libogg:$env:platform-windows
+              libtheora:$env:platform-windows
+              ffmpeg:$env:platform-windows
+#vcpkg export  glib:$env:platform-windows
+#              libogg:$env:platform-windows
+#              libtheora:$env:platform-windows
+#              ffmpeg:$env:platform-windows --zip
+#cp c:\Tools\vcpkg\"vcpkg-export-*.zip" $env:APPVEYOR_BUILD_FOLDER
 
 # vcpkg: integrate
 cd c:\tools\vcpkg
 vcpkg integrate install
 vcpkg list --triplet x64-windows
-cd %APPVEYOR_BUILD_FOLDER%
+cd $env:APPVEYOR_BUILD_FOLDER
