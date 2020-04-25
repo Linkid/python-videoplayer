6c32b30da94e171822ccf55b0938f13911730342
François Magimel <magimel.francois@gmail.com>
2020-04-25 18:37:07 +0200

    PS: call the varsall script to set the path

--- .appveyor/before_build.ps1
+++ .appveyor/before_build.ps1
@@ -1,22 +1,15 @@
 # before build on Windows
 
-# PATH: add dumpbin
+# set the arch for VS 2015
 # https://www.appveyor.com/docs/lang/cpp/
-$env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin"
-$env:VCPKG_BUILD = 1
-echo "${env:PATH}"
-
-# set the arch
-#if ($env:platform -eq "x64") {
-#    & "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-#    & "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
-#} else {
-#    & "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-#}
+if ($env:platform -eq "x64") {
+    "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
+    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+} else {
+    "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+}
 
 # vcpkg: install dependencies
-echo "${env:platform}-windows"
-ls env:
 vcpkg install glib:"${env:platform}"-windows
 vcpkg install libogg:"${env:platform}"-windows
 vcpkg install libtheora:"${env:platform}"-windows
@@ -34,4 +27,7 @@ vcpkg list --triplet '"${env:platform}"-windows'
 cd $env:APPVEYOR_BUILD_FOLDER
 
 # set var
+$env:VCPKG_BUILD = 1
 $env:VCPKG_TOOLCHAIN="C:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake"
+echo "${env:PATH}"
+ls env:
