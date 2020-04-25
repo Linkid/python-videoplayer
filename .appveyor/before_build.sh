edd40131684e5d127ee2f2d10112377df31207b4
François Magimel <magimel.francois@gmail.com>
2020-04-25 22:50:50 +0200

    Tests in bash

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -1,14 +1,14 @@
 # before build
 
 
-if [ $APPVEYOR_BUILD_WORKER_IMAGE = "Ubuntu" ]
+if [[ $APPVEYOR_BUILD_WORKER_IMAGE == "Ubuntu" ]]
 then
     # linux
     echo "linux"
     sudo apt-get -qq update
     sudo apt-get install -y freeglut3-dev libogg-dev libtheora-dev libswscale-dev
 
-elif [ $APPVEYOR_BUILD_WORKER_IMAGE = "macos-mojave" ]
+elif [[ $APPVEYOR_BUILD_WORKER_IMAGE == "macos-mojave" ]]
 then
     # macos
     echo "osx"
