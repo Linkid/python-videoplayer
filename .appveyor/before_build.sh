5f006f1418d797409f13d53c5720d508cf20992c
François Magimel <magimel.francois@gmail.com>
2020-04-25 22:45:38 +0200

    debugs

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -4,12 +4,14 @@
 if [ $APPVEYOR_BUILD_WORKER_IMAGE = "Ubuntu" ]
 then
     # linux
+    echo "linux"
     sudo apt-get -qq update
     sudo apt-get install -y freeglut3-dev libogg-dev libtheora-dev libswscale-dev
 
 elif [ $APPVEYOR_BUILD_WORKER_IMAGE = "macos-mojave" ]
 then
     # macos
+    echo "osx"
     brew update
     brew install glib freeglut libogg theora ffmpeg
 fi
