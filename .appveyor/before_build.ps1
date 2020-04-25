577889c750294e6413eb75ad39e48c9f2c5f4c0d
François Magimel <magimel.francois@gmail.com>
2020-04-25 19:11:09 +0200

    No multiline

--- .appveyor/before_build.ps1
+++ .appveyor/before_build.ps1
@@ -4,12 +4,9 @@
 # https://www.appveyor.com/docs/lang/cpp/
 # if ($env:APPVEYOR_BUILD_WORKER_IMAGE -eq "Visual Studio 2015") {
 if ($env:platform -eq "x64") {
-    cmd.exe /c "call `"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd`" /x64 &&
-                call `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat`" x86_amd64 &&
-                set > %temp%\vcvars.txt"
+    cmd.exe /c "call `"C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd`" /x64 && call `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat`" x86_amd64 && set > %temp%\vcvars.txt"
 } else {
-    cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat`" x86 &&
-                set > %temp%\vcvars.txt"
+    cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat`" x86 && set > %temp%\vcvars.txt"
 }
 
 # set varsall vars
