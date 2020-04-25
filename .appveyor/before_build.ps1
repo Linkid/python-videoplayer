ec10402d1e962002565eca1eb3b909b6df36bf99
François Magimel <magimel.francois@gmail.com>
2020-04-25 17:28:23 +0200

    PS: reuse vcpkg install on each line

--- .appveyor/before_build.ps1
+++ .appveyor/before_build.ps1
@@ -13,22 +13,22 @@ if ($env:platform -eq "x64") {
 } else {
     set varsall="x86"
 }
-& "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /${env:platform}
+echo varsall
+#& "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /${env:platform}
 & "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" $env:varsall
 
 # vcpkg: install dependencies
 echo "${env:platform}-windows"
 ls env:
-vcpkg install glib:"${env:platform}"-windows {
-              libogg:"${env:platform}"-windows
-              libtheora:"${env:platform}"-windows
-              ffmpeg:"${env:platform}"-windows
-}
+vcpkg install glib:"${env:platform}"-windows
+vcpkg install libogg:"${env:platform}"-windows
+vcpkg install libtheora:"${env:platform}"-windows
+vcpkg install ffmpeg:"${env:platform}"-windows
 #vcpkg export  glib:${env:platform}-windows
 #              libogg:${env:platform}-windows
 #              libtheora:${env:platform}-windows
 #              ffmpeg:${env:platform}-windows --zip
-#cp c:\Tools\vcpkg\"vcpkg-export-*.zip" $env:APPVEYOR_BUILD_FOLDER --output=vcpkg-export.zip
+#cp c:\Tools\vcpkg\"vcpkg-export-*.zip" --output=vcpkg-export.zip $env:APPVEYOR_BUILD_FOLDER
 
 # vcpkg: integrate
 cd c:\tools\vcpkg
