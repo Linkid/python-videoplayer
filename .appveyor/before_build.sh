2bc40415ddaa2828e1adc17e6e7617c915cbe91b
François Magimel <magimel.francois@gmail.com>
2020-04-26 18:53:13 +0200

    Linux: install freeglut instead of mesa

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -4,7 +4,7 @@ if [[ `uname` == "Linux" ]]
 then
     # linux (centos 6)
     echo "yyyy `cat /etc/redhat-release` (`arch`)"
-    yum -y install glib2-devel mesa-libGL-devel libogg-devel libtheora-devel
+    yum -y install glib2-devel freeglut-devel libogg-devel libtheora-devel
     rpm --Uvh http://www.nosuchhost.net/~cheese/fedora/packages/epel-6/$basearch/cheese-release-6-1.noarch.rpm
     yum -y install libswscale-devel
     echo "zzzz $basearch - $releasever."
