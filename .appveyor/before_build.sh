63ad43303e29e13ac1bb26207d6c095d9e7760a8
François Magimel <magimel.francois@gmail.com>
2020-04-26 18:41:30 +0200

    Linux: install deps with yum

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -2,9 +2,18 @@
 
 if [[ `uname` == "Linux" ]]
 then
-    # linux
-    echo "xxx linux"
-    yum -y install glib2-devel mesa-libGL-devel libogg-devel libtheora-devel libswscale-devel
+    # linux (centos 6)
+    echo "yyyy $PROCESSOR_ARCHITECTURE"
+    if [[ $PROCESSOR_ARCHITECTURE == "x64" ]]
+    then
+        archi=x86_64
+    else
+        archi=i386
+    fi
+    yum -y install glib2-devel mesa-libGL-devel libogg-devel libtheora-devel
+    rpm --Uvh http://www.nosuchhost.net/~cheese/fedora/packages/epel-6/$archi/cheese-release-6-1.noarch.rpm
+    yum -y install libswscale-devel
+    echo "zzzz $PROCESSOR_ARCHITECTURE"
 
 elif [[ $APPVEYOR_BUILD_WORKER_IMAGE == "macos-mojave" ]]
 then
