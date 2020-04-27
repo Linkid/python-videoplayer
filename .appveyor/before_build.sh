f14ce84ffe95757c697f79e446d0cfe253b3d88f
François Magimel <magimel.francois@gmail.com>
2020-04-27 11:42:00 +0200

    Windows: fix paths

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -31,10 +31,11 @@ then
     echo $platform
     if [[ $platform == "AMD64" ]]
     then
-        "C:\Program Files\Microsoft SDKs\Windows\v7.1\Bin\SetEnv.cmd" /x64
-        "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86_amd64
+        platform=x64
+        C:/Program\ Files/Microsoft\ SDKs/Windows/v7.1/Bin/SetEnv.cmd /x64
+        C:/Program\ Files \(x86\)/Microsoft\ Visual\ Studio\ 14.0/VC/vcvarsall.bat x86_amd64
     else
-        "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+        C:/Program\ Files \(x86\)/Microsoft\ Visual\ Studio\ 14.0/VC/vcvarsall.bat x86
     fi
 
     vcpkg install glib:${platform}-windows \
