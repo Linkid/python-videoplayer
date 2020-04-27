6e2a47204422b2e2ed177ebccc671b8e44d6aeda
François Magimel <magimel.francois@gmail.com>
2020-04-27 13:11:20 +0200

    Windows: add debugs

--- .appveyor/before_build.sh
+++ .appveyor/before_build.sh
@@ -32,10 +32,15 @@ then
     if [[ $platform == "AMD64" ]]
     then
         platform=x64
+        echo 0
         C:/"Program Files"/Microsoft\ SDKs/Windows/v7.1/Bin/SetEnv.cmd /x64
+        echo 1
         C:/Program\ Files \(x86\)/Microsoft\ Visual\ Studio\ 14.0/VC/vcvarsall.bat x86_amd64
+        echo 2
     else
-        C:/Program\ Files \(x86\)/Microsoft\ Visual\ Studio\ 14.0/VC/vcvarsall.bat x86
+        echo 3
+        C:/"Program Files (x86)"/Microsoft\ Visual\ Studio\ 14.0/VC/vcvarsall.bat x86
+        echo 4
     fi
 
     vcpkg install glib:${platform}-windows \
