49974051bad8c8fed4be450804fdc01c1f642da6
François Magimel <magimel.francois@gmail.com>
2020-05-01 22:05:20 +0200

    Comment CMAKE_C_COMPILER

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ if os.getenv("VCPKG_BUILD"):
     build_cmake_args.append('-DCMAKE_TOOLCHAIN_FILE={}'.format(os.getenv("VCPKG_TOOLCHAIN")))
     # C:/Program Files (x86)/Microsoft Visual Studio 14.0/VC/bin/x86_amd64/cl.exe
     # C:/Users/appveyor/AppData/Local/Programs/Common/Microsoft/Visual C++ for Python/9.0/VC/bin/cl.exe
-    build_cmake_args.append('-DCMAKE_C_COMPILER="cl.exe"')
+    #build_cmake_args.append('-DCMAKE_C_COMPILER="cl.exe"')
     #build_cmake_args.append('-DCMAKE_C_COMPILER=clang-cl.exe')
 
     print("****")
