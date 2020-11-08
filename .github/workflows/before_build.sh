4079f6b4791b683b37a058c2b39bc2753c995e73
François Magimel <magimel.francois@gmail.com>
2020-11-08 19:32:07 +0100

    buid-cibuildwheel: init build & install dependencies before the build

---
+++ .github/workflows/before_build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# before build
+
+set -e
+
+# get the operating system
+operating_system=$1
+
+#
+# install system dependencies
+#
+case $operating_system in
+
+    "linux")
+        # linux (centos 6)
+        # arch
+        echo "`cat /etc/redhat-release` (`arch`)"
+        if [[ `arch` == 'i686' ]]
+        then
+            basearch=i386
+        else
+            basearch=x86_64
+        fi
+
+        # install
+        yum -y install \
+            glib2-devel \
+            freeglut-devel \
+            libogg-devel \
+            libtheora-devel
+        rpm -Uvh http://www.nosuchhost.net/~cheese/fedora/packages/epel-6/$basearch/cheese-release-6-1.noarch.rpm
+        yum -y install libswscale-devel
+    ;;
+
+    "macos")
+        # macos
+        echo "osx"
+        brew update
+        brew install \
+            glib \
+            libogg \
+            theora \
+            ffmpeg
+    ;;
+
+    *)
+        echo ${operating_system}
+    ;;
+esac
+
+#
+# install python dependencies
+#
+pip install cython scikit-build cmake ninja
