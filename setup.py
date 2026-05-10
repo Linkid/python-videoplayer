c738ccb6adf27beeeb794760ed0b41d52079f82b
François Magimel <magimel.francois@gmail.com>
2026-05-10 14:13:38 +0200

    ci: drop support for python 3.7, 3.8 and 3.9
    Those Python versions are not maintained anymore.
    Python 2.7 is now optional in tests.

--- setup.py
+++ setup.py
@@ -71,6 +71,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Multimedia',
         'Topic :: Software Development :: Libraries',
     ],
